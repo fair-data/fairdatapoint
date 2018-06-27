07a5966738a208f53b8cd2be480a2c5a2fca48cd
arnikz <a.kuzniar@esciencecenter.nl>
2018-06-27 16:27:07 +0200

    Fix tests

--- fdp-api/python/tests/test_metadata.py
+++ fdp-api/python/tests/test_metadata.py
@@ -1,12 +1,12 @@
 import sys
 import six
 
-# add parent folder to path
-sys.path.append('../')
-
-
 from nose.tools import (assert_equals, assert_true, assert_false)
 from metadata import (FAIRConfigReader, FAIRGraph)
+
+# add parent folder to path
+#sys.path.append('../')
+
 
 if six.PY2:
     from urllib2 import urlparse
@@ -18,8 +18,9 @@ reader = FAIRConfigReader()
 
 
 def test_sections():
-    set_a = set(
-       ['fdp', 'catalog/catalog-01', 'dataset/breedb', 'distribution/breedb-sqldump', 'distribution/breedb-sparql'])
+    set_a = set(['fdp', 'catalog/astron-01', 'dataset/lofar-lta-dbview',
+                 'distribution/lofar-lta-dbview-sparql',
+                 'distribution/lofar-lta-dbview-sqldump'])
     set_b = set(reader.getSectionHeaders())
     assert_true(set_a == set_b)
 
@@ -53,12 +54,20 @@ def test_fdp_uri():
 
 
 def test_catalog_uri():
-    assert_equals(urljoin(base_uri, 'catalog/catalog-01'), g.catURI('catalog-01'))
+    assert_equals(urljoin(base_uri, 'catalog/astron-01'),
+                  g.catURI('astron-01'))
 
 
 def test_dataset_uri():
-    assert_equals(urljoin(base_uri, 'dataset/breedb'), g.datURI('breedb'))
+    assert_equals(urljoin(base_uri, 'dataset/lofar-lta-dbview'),
+                  g.datURI('lofar-lta-dbview'))
 
 
 def test_distribution_uri():
-    assert_equals(urljoin(base_uri, 'distribution/breedb-sqldump'), g.distURI('breedb-sqldump'))
+    assert_equals(urljoin(base_uri, 'distribution/lofar-lta-dbview-sparql'),
+                  g.distURI('lofar-lta-dbview-sparql'))
+
+
+def test_distribution_uri():
+    assert_equals(urljoin(base_uri, 'distribution/lofar-lta-dbview-sqldump'),
+                  g.distURI('lofar-lta-dbview-sqldump'))
