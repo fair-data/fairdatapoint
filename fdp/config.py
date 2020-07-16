6b6b955f3a5a5f08c523761dcb3741890528a002
Cunliang Geng <c.geng@esciencecenter.nl>
2020-07-16 17:55:19 +0200

    Create config.py

---
+++ fdp/config.py
@@ -0,0 +1,23 @@
+
+from fdp.fairgraph import FAIRGraph
+
+def build_base_uri(host, port):
+    if host == 'localhost':
+        host = 'http://127.0.0.1'
+    elif not host.startswith('http'):
+        host = f'http://{host}'
+    if int(port) == 80:
+        base_uri = host
+    else:
+        base_uri = f'{host}:{port}'
+    return base_uri
+
+_fairgraph = None
+
+def init_fairgraph(host, port, endpoint):
+    base_uri = build_base_uri(host, port)
+    global _fairgraph
+    _fairgraph = FAIRGraph(base_uri, endpoint)
+
+def get_fairgraph():
+    return _fairgraph
