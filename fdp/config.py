e930b2be1731af8f22bb1a7960ecf31369b7648c
Cunliang Geng <c.geng@esciencecenter.nl>
2020-08-11 08:22:11 +0200

    Update config.py with sparql and grlc endpoints

--- fdp/config.py
+++ fdp/config.py
@@ -13,11 +13,25 @@ def build_base_uri(host, port):
     return base_uri
 
 _fairgraph = None
+_grlc_endpoint = None
+_sparql_endpoint = None
 
-def init_fairgraph(host, port, endpoint):
+def init_fairgraph(host, port, sparql_endpoint):
     base_uri = build_base_uri(host, port)
     global _fairgraph
-    _fairgraph = FAIRGraph(base_uri, endpoint)
+    _fairgraph = FAIRGraph(base_uri, sparql_endpoint)
 
 def get_fairgraph():
     return _fairgraph
+
+def init_grlc(grlc_endpoint, sparql_endpoint):
+    global _grlc_endpoint
+    _grlc_endpoint = grlc_endpoint + '/api-local'
+    global _sparql_endpoint
+    _sparql_endpoint = sparql_endpoint
+
+def get_grlc_endpoint():
+    return _grlc_endpoint
+
+def get_sparql_endpoint():
+    return _sparql_endpoint
