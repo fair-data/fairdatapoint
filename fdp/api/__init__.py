ed3674358e901d2baced09b6094f828f2f3af898
Cunliang Geng <c.geng@esciencecenter.nl>
2020-08-11 08:21:40 +0200

    Update api __init__.py

--- fdp/api/__init__.py
+++ fdp/api/__init__.py
@@ -1,1 +1,4 @@
 from .metadata import FDP, Catalog, Dataset, Distribution
+from .search import (FindText, FindURI, FindMediaType, FindPublisher,
+    FindVersion, GetChildrenIDs, GetDataURLs, GetLicenses, GetMediaTypes,
+    GetParentIDs, GetPublishers, GetThemeTaxonomy, GetThemes)
